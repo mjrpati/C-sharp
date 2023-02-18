138e323be8f4a342c12f7ed5b92cf1548d3ea16f
Patricia~ <Your Email>
2023-02-18 19:05:14 -0300

    new

---
+++ AREA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double largura, comprimento, precoMetroQuadrado, area, preco;
+            
+            largura = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            comprimento = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            precoMetroQuadrado = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            area = largura * comprimento;
+            preco = area * precoMetroQuadrado;
+
+            Console.WriteLine("AREA : " +area.ToString("F2", CultureInfo.InvariantCulture)) ;
+            Console.WriteLine("PREÇO : " +preco.ToString("F2", CultureInfo.InvariantCulture)) ;
+        }
+    }
+}
---
+++ Salario/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class MySpace
+    {
+        static void Main(string[] args)
+        
+
+        {
+            string nome;
+            double salariofixo, vendas, m;
+
+            nome = Console.ReadLine();
+            salariofixo = double.Parse(Console.ReadLine());
+            vendas = double.Parse(Console.ReadLine());
+
+            m = vendas * 15 /100 + salariofixo;
+
+            Console.WriteLine("TOTAL = R$ " + m.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+
+    }
+    
+}
+
